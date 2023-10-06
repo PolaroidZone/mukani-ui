a115d6f57ab3fb4b2fcc7edf1d45849e25898112
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-10-06 17:45:37 +0200

    Creating about components

---
+++ src/components/about/AboutMessage.tsx
@@ -0,0 +1,21 @@
+const AboutMessage = () => {
+  return (
+    <div className="about-about">
+      <p>
+        Welcome to my portfolio website! I am a versatile developer with
+        expertise in web development and Android mobile application development.
+        With a primary focus on Android, I create innovative and user-friendly
+        mobile applications that cater to diverse needs. I possess a strong
+        understanding of Node.js, and I am proud to be certified by Edureka.
+        Alongside my development skills, I have a practical understanding of
+        Database Management Systems (DBMS), encompassing both NoSQL and MySQL. I
+        strive to deliver efficient and scalable solutions that enhance user
+        experiences and meet business objectives. Feel free to explore my
+        projects and get in touch to discuss how we can collaborate on your next
+        exciting venture.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMessage;
