e7cc1f4eab0f08a77e2d7b1889ce98869ac93412
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-11-04 00:13:53 +0200

    Addded routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
+import { Route, Routes } from 'react-router-dom';
 import Header from "./components/header/Header";
 import Footer from "./components/footer/footer";
 import Home from "./pages/Home";
+import Articles from "./pages/Article";
+import Projects from "./pages/Projects";
 
 import "./styles/App.css";
 
 function App() {
-const [page, setPage] = useState("Home")
-
-let Component = Home;
-if (page === "Home") {
-  Component = Home;
-}
-
   return (
     <div>
       <Header/>
-      <Component/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/projects" element={<Projects/>}></Route>
+        <Route path="/article" element={<Articles/>}></Route>
+      </Routes>
       <Footer/>
     </div>
   );
