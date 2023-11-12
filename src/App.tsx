25694dfe537fca3c49e70e88113129621c2ca75e
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-11-12 14:53:13 +0200

    Added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import { useState } from "react";
-import { Route, Routes } from 'react-router-dom';
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+
 import Header from "./components/header/Header";
 import Footer from "./components/footer/footer";
-import Home from "./pages/Home";
-import Articles from "./pages/Article";
-import Projects from "./pages/Projects";
-
 import "./styles/App.css";
+
+const Home = lazy(() => import("./pages/Home"));
+const Articles = lazy(() => import("./pages/Article"));
+const Projects = lazy(() => import("./pages/Projects"));
 
 function App() {
   return (
     <div className="">
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/projects" element={<Projects/>}></Route>
-        <Route path="/article" element={<Articles/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/article" element={<Articles />}></Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
-} 
+}
 
 export default App;
