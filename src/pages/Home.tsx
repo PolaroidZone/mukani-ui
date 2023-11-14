b9413758f4f1a9a3d7ad946d59fad9849845e12e
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-11-14 03:28:08 +0200

    Called endpoint

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Hero from "../components/hero/Hero";
 import About from "../components/about/About";
@@ -7,8 +7,21 @@ import ContactSection from "../components/contact/ContactSection";
 import BlogSection from "../components/blog/BlogSection";
 import ProjectsSection from "../components/projects/ProjectsSection";
 
+import { getHomeData } from "../services/endpoints";
+
 const Home = () => {
   const [isloading, setisloading] = useState("true");
+
+  useEffect(() => {
+    getHomeData()
+      .then((res) => {
+        console.log(res);
+        setisloading("false");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <>
