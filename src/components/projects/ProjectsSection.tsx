7c62a71ca7d4cf568e16e1fd3210faf2ea158ad0
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-12-01 15:15:27 +0200

    upating projects section

--- src/components/projects/ProjectsSection.tsx
+++ src/components/projects/ProjectsSection.tsx
@@ -1,32 +1,38 @@
-import ProjectsHeder from "./ProjectsHeader";
+import React from "react";
+import ProjectsHeader from "./ProjectsHeader";
 import Project from "./Project";
-import ProjectsNava from "./ProjectsNav";
+import ProjectsNav from "./ProjectsNav";
 
-interface Props {
-  project: {
-    id: number;
-    title: string;
-    description: string;
-    image: string;
-    url: string;
-  }[];
+interface ProjectData {
+  // Define the structure of your project data
+  id: string;
+  // Add other properties as needed
 }
 
-const ProjectsSection = ({ project }: Props) => {
+interface ProjectsSectionProps {
+  projects: ProjectData[];
+}
 
+const ProjectsSection: React.FC<ProjectsSectionProps> = ({ projects }) => {
+  console.log(projects)
   return (
     <section id="projects" className="container">
       <div className="projects-con" id="project-con">
         <div className="projects-row">
-          <ProjectsHeder />
-          <Project />
+          <ProjectsHeader />
+          {projects.map((project) => (
+            <Project key={project.id} {...project} />
+          ))}
         </div>
         <div className="projects-row2">
-          <Project />
-          <ProjectsNava />
+          {projects.map((project) => (
+            <Project key={project.id} {...project} />
+          ))}
+          <ProjectsNav />
         </div>
       </div>
     </section>
   );
 };
+
 export default ProjectsSection;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,42 @@
 import { useEffect, useState } from "react";
-
 import Hero from "../components/hero/Hero";
 import About from "../components/about/About";
 import Skills from "../components/skills/Skills";
 import ContactSection from "../components/contact/ContactSection";
 import BlogSection from "../components/blog/BlogSection";
 import ProjectsSection from "../components/projects/ProjectsSection";
-
 import { getHomeData } from "../services/endpoints";
 
 const Home = () => {
-  const [isloading, setisloading] = useState("true");
-
-  const [projects, setProjects] = useState({});
-  const [articlesDAta, setArticlesDAta] = useState({});
+  const [isloading, setisloading] = useState(true);
+  const [projects, setProjects] = useState([]);
+  const [articlesData, setArticlesData] = useState([]);
 
   useEffect(() => {
-    getHomeData()
-      .then((res) => {
-        console.log(res);
-        const { projects, articlesDAta } = res.data;
-        setProjects(projects);
-        setArticlesDAta(articlesDAta);
-        setisloading("false");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+    if (isloading) {
+      fetchData();
+    }
+  }, [isloading]);
+
+  const fetchData = async () => {
+    try {
+      const res = await getHomeData();
+      console.log(res);
+      const { projects, articles } = res.data;
+      setProjects(projects);
+      setArticlesData(articles);
+      setisloading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
       <Hero />
       <About />
       <Skills />
-      <ProjectsSection project={[]} />
+      <ProjectsSection projects={projects} />
       <BlogSection />
       <ContactSection />
     </>
