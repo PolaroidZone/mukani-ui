fa5298168b94ffe1c2b4875e5fb215503c6f567f
Tsogang Mosweswe <68607674+PolaroidZone@users.noreply.github.com>
2024-11-12 13:37:13 +0200

    Floating hero avata

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ const Home = () => {
       <Hero />
       <About />
       <Skills />
-      <ProjectsSection/>
+      <ProjectsSection />
       <BlogSection />
       <ContactSection />
     </>
